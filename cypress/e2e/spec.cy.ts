c2b169b0d430f1c050a3c37e4dd531da806bf604
Bhargava Reddy Chinthareddy <114492951+Chinthareddy21@users.noreply.github.com>
2023-10-29 12:41:37 +0530

    Intial commit

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -11,6 +11,9 @@ it('Google', () => {
     // Checking visibility of search box and asserting
     .should('be.visible').and('have.id', 'APjFqb')
 
-    // Entering desired text for search and clicking enter to search 
-    .type("mountain{enter}")
+    // Entering desired text for search 
+    .type("mountain")
+  
+    // Clicking enter to search
+    .type('{enter}')
 })
